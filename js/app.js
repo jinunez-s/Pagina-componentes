092b1255862df05b2bad6ce15f6efea44498d774
Julio Nuñez <julionr14@gmail.com>
2021-09-10 22:33:56 -0600

    Website Componentes v1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById("activar-alerta").addEventListener("click", function(){
+    $('#alert-prueba').removeClass("visually-hidden");
+});
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
